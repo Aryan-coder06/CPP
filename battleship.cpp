94559132c7ef95e4ed953919da2d5b578e075144
Dev_Aryan <aryaniiitian06@gmail.com>
2025-06-19 01:16:11 +0530

    Update battleship.cpp

--- battleship.cpp
+++ battleship.cpp
@@ -11,7 +11,7 @@ int main () {
         x = x - 1;
         y = y - 1;
         if(ships[x][y] == 1) {
-            cout << "Ooo  a shot My BROTHER\n" ; 
+            cout << "Ooo  a shot My Brother\n" ; 
         }
         else
         cout << "Missed My BROTHER\n" ;
