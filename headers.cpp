db2480c60945ebe029255ac3c868ce0eae0f86dc
Dev_Aryan <aryaniiitian06@gmail.com>
2025-07-07 18:25:03 +0530

    Update headers.cpp

--- headers.cpp
+++ headers.cpp
@@ -1,4 +1,3 @@
-// C++ program to demonstrate Standard header files
 #include <bits/stdc++.h>
 #include <cmath>
 #include <cstdlib>
@@ -11,25 +10,21 @@ using namespace std;
 
 int main()
 {
-	// using <iostream>
+
 	cout << "Hello, Geek!" << endl;
 
-	// using <cmath>
 	double squareRoot = sqrt(25);
 	cout << "Square root of 25 is: " << squareRoot << endl;
 
-	// using<cstdlib>
-	int randomNum = rand() % 100; // Generate a random
-								// number between 0 and 99
+	int randomNum = rand() % 100; 
+
 	cout << "Random number is : " << randomNum << endl;
 
-	// using <cstring>
 	char mystr1[] = "Hello";
 	char mystr2[] = " World";
 	strcat(mystr1, mystr2);
 	cout << "string after concatenation: " << mystr1 << endl;
 
-	// using <vector>
 	vector<int> numbers = { 1, 2, 3, 4, 5 };
 	cout << "Vector elements are: ";
 	for (const auto& num : numbers) {
@@ -37,7 +32,6 @@ int main()
 	}
 	cout << endl;
 
-	// using <string>
 	string greeting = "Hello, ";
 	string name = "Programmer";
 	string fullGreeting = greeting + name;
